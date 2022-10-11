c792a4b831af5e3b10e6375f134cfbc119d65f03
Emilia Johansson <johansson.emilia@hotmail.se>
2022-10-11 14:37:46 +0200

    expanded the JS code

---
+++ script.js
@@ -0,0 +1,45 @@
+const quiz = document.querySelector(".quiz");
+const submitContainer = document.querySelector(".submit");
+const result = document.querySelector(".result");
+
+const marvelQuiz = [
+    {
+        question: "When was the first Iron Man realesed?",
+        answers: {
+            a: "2008",
+            b: "2010",
+            c: "2005"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What's the name of the raccoon in Guardians of the Galaxy?",
+        answers: {
+            a: "Trash-Panda",
+            b: "Rocket Raccoon",
+            c: "Hamster"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What does Deadpool call his crocs?",
+        answers: {
+            a: "This is some killer crocs!",
+            b: "Flat-ass sandals",
+            c: "Big rubber masturbating boots"
+        },
+        correctAnswer: "c"
+    }
+]
+
+runQuiz(marvelQuiz, quiz, submitContainer, result);
+
+function runQuiz(question, quizContainer, resultContainer, submitButton){
+    
+    function showQuestions(question, quiz){
+
+        const output = [];
+        const answers;
+
+    }
+}
